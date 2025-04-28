f491a65cd407b51f24f01d41338785cdaf4b219b
Fadhail <mochammadfadhail2@gmail.com>
2025-04-28 16:08:59 +0700

    Pertemuan06

---
+++ Pertemuan06/Praktikum/Frontend/my-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { LayoutDashboard } from "./components/layouts/LayoutDashboard";
+import { Dashboard } from "./pages/Dashboard";
+import { MahasiswaPage } from "./pages/MahasiswaPage";
+
+export default function App() {
+  return (
+    <Router>
+      <LayoutDashboard>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/mahasiswa" element={<MahasiswaPage />} />
+        </Routes>
+      </LayoutDashboard>
+    </Router>
+  );
+}
